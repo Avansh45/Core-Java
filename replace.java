5bc31079cb098ff6182c3b5c138c1b9935497824
Avansh Chadgal <avanshchadgalmh25@gmail.com>
2025-06-19 21:41:56 +0530

    Basic String Program

--- replace.java
+++ replace.java
@@ -3,7 +3,6 @@ public class replace {
     public static void main(String[] args) {
         String str = "Java is Programming Language";
         System.out.println("original String : "+str);
-
         str = str.replace(" ","_");
         System.out.println("\nReplaced String : "+str);
     }
