d326f99d3c18e227a7f0820fd36bf563d5a59a17
Avansh Chadgal <avanshchadgalmh25@gmail.com>
2025-10-17 15:22:29 +0530

    Updated FirstElement Prog

--- CollectionsFramework/Sets/FirstElement.java
+++ CollectionsFramework/Sets/FirstElement.java
@@ -2,18 +2,27 @@ package CollectionsFramework.Sets;
 
 import java.util.LinkedHashSet;
 import java.util.Arrays;
+import java.util.Iterator;
 
-// Given an array of integers or strings, find the first element that occurs only once using Set
+// Given an array of integers or strings , find the first element that occurs only once using Set.
 public class FirstElement{
     public static void main(String[] args) {
         
-        String[] fruits = {"Apple", "Banana", "Mango", "Orange", "Banana", "Apple", "Grapes", "Mango"};
+        String[] fruits = {"Orange", "Apple", "Banana", "Mango",  "Banana", "Apple", "Grapes", "Mango"};
 
 
         LinkedHashSet<String> set = new LinkedHashSet<>(Arrays.asList(fruits));
-        
+        for(String strs : fruits){
+            set.add(strs);
+        }
 
+        Iterator<String> it = set.iterator();
+        if(it.hasNext()){
+            System.out.println("First Element : "+it.next());
+        }
+        else{
+            System.out.println("Set is Empty !");
+        }
         
-
     }
 }
